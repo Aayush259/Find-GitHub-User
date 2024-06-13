5346bf0139fe2e36dcebbd4ce98630c6acca5a27
Square <aayushk4980@gmail.com>
2024-06-13 22:35:35 +0530

    Setup react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/Find-GitHub-User/' element={<App />} >
+      <Route path='/Find-GitHub-User/' element={<App />} />
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 );
