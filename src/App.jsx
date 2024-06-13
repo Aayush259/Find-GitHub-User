33d0069cc3300cb64568189241cc7bfa37a7460e
Square <aayushk4980@gmail.com>
2024-06-13 22:07:44 +0530

    Added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
 export default function App() {
 
   return (
     <>
-    Find GitHub User
+    <div className='bg-black min-h-screen overflow-x-hidden flex flex-col'>
+      <Header />
+    </div>
     </>
   );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function Header() {
+
+    return (
+        <div className='
+            my-4 mt-10 mx-auto
+            max-w-xl w-[97%]
+            p-4
+            flex justify-between items-center
+            bg-slate-900
+            rounded-md
+            text-white text-xl font-normal tracking-wide'
+        >
+            <h1>
+                Search GitHub User
+            </h1>
+
+            <a className='cursor-pointer' href='https://github.com/Aayush259' target='_blank'>
+                <img 
+                    src='https://avatars.githubusercontent.com/u/159794430?v=4' 
+                    alt='Aayush' 
+                    className='
+                        h-9 rounded-full
+                        hover:opacity-80'
+                />
+            </a>
+        </div>
+    );
+};
