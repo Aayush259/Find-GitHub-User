1f0422dfc46727506b14d8b0169c71432b352565
Square <aayushk4980@gmail.com>
2024-06-15 02:52:50 +0530

    Abort fetch when component unmounts

--- src/components/DisplayUserInfo.jsx
+++ src/components/DisplayUserInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import GitHubUserInfo from './GithubUserInfo';
 import Loader from './Loader.jsx';
@@ -13,19 +13,33 @@ export default function DisplayUserInfo() {
     // Data state.
     const [Data, setData] = useState(null);
 
+    // Controller reference to abort the fetch request when required.
+    const controllerRef = useRef(null);
+
     // State for return value.
     const [returnValue, setReturnValue] = useState(<Loader />);
 
     useEffect(() => {
 
+        // Initialize abort controller.
+        const controller = new AbortController();
+        controllerRef.current = controller;
+
         // URL
         const url = `https://api.github.com/users/${username}`;
         
         // Fetching Data
-        fetch(url)
+        fetch(url, { signal: controller.signal })
         .then(response => response.json())
         .then(data => setData(data))
         .catch(err => console.log('Error:', err));
+
+        // Cleanup and abort fetch if component unmounts.
+        return () => {
+            if (controllerRef.current) {
+                controllerRef.current.abort();
+            }
+        };
 
     }, [username]);
 
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Loader() {
 
     return (
-        <div className='flex space-x-2 justify-center items-center bg-transparent h-[80vh]'>
+        <div className='flex space-x-2 justify-center items-center bg-slate-800 h-[80vh] max-w-4xl w-[95%] rounded-b-2xl mb-4'>
             <span className='sr-only'>Loading...</span>
             <div className='h-6 w-6 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
             <div className='h-6 w-6 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
